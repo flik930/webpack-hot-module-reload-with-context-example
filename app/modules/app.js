ac123eccd9d262fe6dfe1423303596bbacb4cf4d
Steve Lam <steve.lam@gravitysupplychain.com>
2017-10-03 11:57:54 +0800

    clean code again

--- app/modules/app.js
+++ app/modules/app.js
@@ -4,18 +4,10 @@ import ReactDOM from 'react-dom';
 import A from 'a'
 
 ReactDOM.render(<A/>, document.getElementById('hello'))
-
 var context = require.context("react", true, /\.js$/);
-
 if (module.hot) {
   module.hot.accept(context.id, function () {
-    //You can't use context here. You _need_ to call require.context again to
-    //get the new version. Otherwise you might get errors about using disposed
-    //modules
     var reloadedContext = require.context("react", true, /\.js$/);
-    //To find out what module was changed you just compare the result of the
-    //require call with the version stored in the modules hash using strict
-    //equality. Equal means it is unchanged.
     ReactDOM.render(<A/>, document.getElementById('hello'))
   });
 }
