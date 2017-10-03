aedb1da5e00ab4e4696d324cc47c3cb37c114e18
Steve Lam <steve.lam@gravitysupplychain.com>
2017-10-03 11:41:14 +0800

    clean up code

--- app/modules/app.js
+++ app/modules/app.js
@@ -6,12 +6,6 @@ import A from 'a'
 ReactDOM.render(<A/>, document.getElementById('hello'))
 
 var context = require.context("react", true, /\.js$/);
-var modules = {};
-context.keys().forEach(function (key) {
-  var module = context(key);
-  modules[key] = module;
-  customReloadLogic(key, module, false);
-})
 
 if (module.hot) {
   module.hot.accept(context.id, function () {
@@ -22,20 +16,6 @@ if (module.hot) {
     //To find out what module was changed you just compare the result of the
     //require call with the version stored in the modules hash using strict
     //equality. Equal means it is unchanged.
-    var changedModules = reloadedContext.keys()
-      .map(function (key) {
-        return [key, reloadedContext(key)];
-      })
-      .filter(function (reloadedModule) {
-        return modules[reloadedModule[0]] !== reloadedModule[1];
-      });
-    changedModules.forEach(function (module) {
-      modules[module[0]] = module[1];
-      customReloadLogic(module[0], module[1], true);
-    });
+    ReactDOM.render(<A/>, document.getElementById('hello'))
   });
-}
-
-function customReloadLogic(name, module, isReload) {
-  console.log("module " + name + (isReload ? " re" : " ") + "loaded");
 }
